36713781914ede04ef89bf161e88a0e27e4814fb
Piotr Solnica <piotr.solnica.ps@gmail.com>
2023-01-18 01:35:10 +0100

    function modified

--- js/script.js
+++ js/script.js
@@ -1,35 +1,52 @@
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let formElement = document.querySelector(".js-form");
-let resultElement = document.querySelector(".js-result");
+{
+    const calculateResult = (amount, currency) => {
+        const rateEUR = 4.67;
+        const rateUSD = 4.38;
+        const rateGBP = 5.27;
 
-const rateEUR = 4.67;
-const rateUSD = 4.38;
-const rateGBP = 5.27;
+        switch (currency) {
+            case "EUR":
+                return amount / rateEUR;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let amount = amountElement.value;
-    let currency = currencyElement.value;
+            case "USD":
+                return amount / rateUSD;;
 
-    let rate;
-
-    switch (currency) {
-        case "EUR":
-            rate = rateEUR;
-            break;
-
-        case "USD":
-            rate = rateUSD;
-            break;
-
-        case "GBP":
-            rate = rateGBP;
+            case "GBP":
+                return amount / rateGBP;
+        }
     }
-    const result = amount / rate
-    resultElement.innerHTML = result.toFixed(2);
-});
 
-formElement.addEventListener("reset", () => {
-    resultElement.innerText = "N/A";
-});
+    const formElement = document.querySelector(".js-form");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+        const resultElement = document.querySelector(".js-result");
+
+        const amount = amountElement.value;
+        const currency = currencyElement.value;
+
+        const result = calculateResult(amount, currency);
+        resultElement.innerHTML = result.toFixed(2);
+    }
+
+    const onFormReset = () => {
+
+        const resultElement = document.querySelector(".js-result");
+
+        resultElement.innerText = "N/A";
+    }
+    const init = () => {
+
+        formElement.addEventListener("submit", onFormSubmit)
+    }
+
+    const deleteResult = () => {
+        formElement.addEventListener("reset", onFormReset)
+    }
+
+    init();
+    deleteResult();
+}
